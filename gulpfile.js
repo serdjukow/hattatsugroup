c1f9f1d79983171fae3103a8e58ca04a1c56bbfb
serdjukow <sergej@serdjukow.eu>
2022-07-19 16:06:23 +0200

    U-5

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,37 @@
 let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
-	fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+		fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
 
-const { src, dest, parallel, series, watch } = require('gulp')
-const browserSync = require('browser-sync').create()
-const bssi = require('browsersync-ssi')
-const ssi = require('ssi')
-const webpack = require('webpack-stream')
-const sass = require('gulp-sass')
-const sassglob = require('gulp-sass-glob')
-const less = require('gulp-less')
-const lessglob = require('gulp-less-glob')
-const styl = require('gulp-stylus')
-const stylglob = require('gulp-noop')
-const cleancss = require('gulp-clean-css')
-const autoprefixer = require('gulp-autoprefixer')
-const rename = require('gulp-rename')
-const imagemin = require('gulp-imagemin')
-const newer = require('gulp-newer')
-const rsync = require('gulp-rsync')
-const del = require('del')
+import pkg from 'gulp'
+const { gulp, src, dest, parallel, series, watch } = pkg
+
+import browserSync   from 'browser-sync'
+import bssi          from 'browsersync-ssi'
+import ssi           from 'ssi'
+import webpackStream from 'webpack-stream'
+import webpack       from 'webpack'
+import TerserPlugin  from 'terser-webpack-plugin'
+import gulpSass      from 'gulp-sass'
+import dartSass      from 'sass'
+import sassglob      from 'gulp-sass-glob'
+const  sass          = gulpSass(dartSass)
+import less          from 'gulp-less'
+import lessglob      from 'gulp-less-glob'
+import styl          from 'gulp-stylus'
+import stylglob      from 'gulp-noop'
+import postCss       from 'gulp-postcss'
+import cssnano       from 'cssnano'
+import autoprefixer  from 'autoprefixer'
+import imagemin      from 'gulp-imagemin'
+import changed       from 'gulp-changed'
+import concat        from 'gulp-concat'
+import rsync         from 'gulp-rsync'
+import del           from 'del'
 
 function browsersync() {
 	browserSync.init({
 		server: {
 			baseDir: 'app/',
-			middleware: bssi({ baseDir: 'app/', ext: '.html' }),
+			middleware: bssi({ baseDir: 'app/', ext: '.html' })
 		},
 		ghostMode: { clicks: false },
 		notify: false,
@@ -35,29 +42,40 @@ function browsersync() {
 
 function scripts() {
 	return src(['app/js/*.js', '!app/js/*.min.js'])
-		.pipe(
-			webpack({
-				mode: 'production',
-				performance: { hints: false },
-				module: {
-					rules: [
-						{
-							test: /\.(js)$/,
-							exclude: /(node_modules)/,
+		.pipe(webpackStream({
+			mode: 'production',
+			performance: { hints: false },
+			plugins: [
+				new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery' }), // jQuery (npm i jquery)
+			],
+			module: {
+				rules: [
+					{
+						test: /\.m?js$/,
+						exclude: /(node_modules)/,
+						use: {
 							loader: 'babel-loader',
-							query: {
-								presets: ['@babel/env'],
-								plugins: ['babel-plugin-root-import'],
-							},
-						},
-					],
-				},
-			})
-		)
-		.on('error', function handleError() {
+							options: {
+								presets: ['@babel/preset-env'],
+								plugins: ['babel-plugin-root-import']
+							}
+						}
+					}
+				]
+			},
+			optimization: {
+				minimize: true,
+				minimizer: [
+					new TerserPlugin({
+						terserOptions: { format: { comments: false } },
+						extractComments: false
+					})
+				]
+			},
+		}, webpack)).on('error', function handleError() {
 			this.emit('end')
 		})
-		.pipe(rename('app.min.js'))
+		.pipe(concat('app.min.js'))
 		.pipe(dest('app/js'))
 		.pipe(browserSync.stream())
 }
@@ -65,20 +83,32 @@ function scripts() {
 function styles() {
 	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
 		.pipe(eval(`${preprocessor}glob`)())
-		.pipe(eval(preprocessor)())
-		.pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] /*grid: false*/ }))
-		.pipe(cleancss({ level: { 1: { specialComments: 0 } }, format: 'beautify' }))
-		.pipe(rename({ suffix: '.min' }))
+		.pipe(eval(preprocessor)({ 'include css': true }))
+		.pipe(postCss([
+			autoprefixer({ grid: 'autoplace' }),
+			cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
+		]))
+		.pipe(concat('app.min.css'))
 		.pipe(dest('app/css'))
 		.pipe(browserSync.stream())
 }
 
 function images() {
-	return src(['app/images/src/**/*']).pipe(newer('app/images/dist')).pipe(imagemin()).pipe(dest('app/images/dist')).pipe(browserSync.stream())
+	return src(['app/images/src/**/*'])
+		.pipe(changed('app/images/dist'))
+		.pipe(imagemin())
+		.pipe(dest('app/images/dist'))
+		.pipe(browserSync.stream())
 }
 
 function buildcopy() {
-	return src(['{app/js,app/css}/*.min.*', 'app/images/**/*.*', '!app/images/src/**/*', 'app/fonts/**/*'], { base: 'app/' }).pipe(dest('dist'))
+	return src([
+		'{app/js,app/css}/*.min.*',
+		'app/images/**/*.*',
+		'!app/images/src/**/*',
+		'app/fonts/**/*'
+	], { base: 'app/' })
+	.pipe(dest('dist'))
 }
 
 async function buildhtml() {
@@ -87,40 +117,34 @@ async function buildhtml() {
 	del('dist/parts', { force: true })
 }
 
-function cleandist() {
-	return del('dist/**/*', { force: true })
+async function cleandist() {
+	del('dist/**/*', { force: true })
 }
 
 function deploy() {
-	return src('dist/').pipe(
-		rsync({
+	return src('dist/')
+		.pipe(rsync({
 			root: 'dist/',
 			hostname: 'username@yousite.com',
 			destination: 'yousite/public_html/',
-			clean: true, // Mirror copy with file deletion
-			include: [
-				/* '*.htaccess' */
-			], // Included files to deploy,
-			exclude: ['**/Thumbs.db', '**/*.DS_Store'],
+			// clean: true, // Mirror copy with file deletion
+			include: [/* '*.htaccess' */], // Included files to deploy,
+			exclude: [ '**/Thumbs.db', '**/*.DS_Store' ],
 			recursive: true,
 			archive: true,
 			silent: false,
-			compress: true,
-		})
-	)
+			compress: true
+		}))
 }
 
 function startwatch() {
 	watch(`app/styles/${preprocessor}/**/*`, { usePolling: true }, styles)
 	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
-	watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images)
+	watch('app/images/src/**/*', { usePolling: true }, images)
 	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
 }
 
-exports.scripts = scripts
-exports.styles = styles
-exports.images = images
-exports.deploy = deploy
-exports.assets = series(scripts, styles, images)
-exports.build = series(cleandist, scripts, styles, images, buildcopy, buildhtml)
-exports.default = series(scripts, styles, images, parallel(browsersync, startwatch))
+export { scripts, styles, images, deploy }
+export let assets = series(scripts, styles, images)
+export let build = series(cleandist, images, scripts, styles, buildcopy, buildhtml)
+export default series(scripts, styles, images, parallel(browsersync, startwatch))
